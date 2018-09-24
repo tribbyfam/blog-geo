16b40fd266a400d421e7206c55a8ab964cc5ce3d
Mac <mac@MacBook-Pro.local>
2018-09-24 14:31:59 -0500

    signup, login, logout works, posts and my-posts pages also work as well as users

--- app.rb
+++ app.rb
@@ -1,11 +1,20 @@
 require 'sinatra'
 require_relative 'models'
 
-set :sessions, true
-use Rack::MethodOverride
+enable :sessions
+
+# set :sessions, true
+# use Rack::MethodOverride
+
+def current_user
+  User.find(session[:user_id])
+end
+
+def current_user_name
+  User.find_by(session[:user_id], first_name: params[:first_name])
+end
 
 get '/' do
- 
   erb :dashboard
 end
 
@@ -23,7 +32,7 @@ post '/signup' do
   birthday: params[:birthdate]
   )
   session[:user_id] = user.id
-  redirect '/posts'
+  redirect '/new_post'
 end
 
 get '/login' do
@@ -35,18 +44,88 @@ post '/login' do
 
   if user && user.userpassword == params[:userpassword]
     session[:user_id] = user.id
-    redirect '/posts'
+    redirect '/my_posts'
   else
     redirect '/signup'
   end
 end
 
+get '/users' do
+  erb :users, locals: { users: User.all }
+end
+
+get '/users/:id' do
+  @users = User.all
+  @users_posts = @users.posts
+  erb :my_posts
+end
+
 get '/posts' do
+  if (session[:user_id].nil?)
+    redirect '/login'
+  else
+    @current_user = current_user
+    @posts = Post.all
+    # @post = Post.find_by(user_id: @current_user.id)
+  end
+  erb :posts
+end
+
+get '/posts/:id' do
+  post = Post.find_by(title: params[:title ])
   erb :posts, locals: { posts: Post.all }
 end
 
-get '/users' do
-  erb :users, locals: { users: User.all }
+get '/my_posts' do
+ 
+  if (session[:user_id].nil?)
+    redirect '/login'
+  else
+    @user = User.find(session[:user_id])
+    @post = Post.where(user_id: session[:user_id])
+    @user = session[:id]
+    erb :my_posts
+  end
+end
+
+get '/new_post' do
+  if (session[:user_id].nil?)
+    redirect '/login'
+  else
+    @current_user = current_user
+  end
+  erb :new_post
+end
+
+post '/new_post' do
+  if (session[:user_id].nil?)
+    redirect '/login'
+  else
+  @user = session[:id]
+  @posts = Post.create(
+    title: params[:title],
+    content: params[:content],
+    user_id: session[:user_id]
+    )
+    redirect '/posts'
+  end
+end
+
+get '/posts/:id/edit' do
+  user = User.find_by(params[:id])
+  erb :edit_post
+end
+
+put '/posts/:id' do
+  user = User.find_by(params[:id])
+  @user_posts = user.post
+  # @user.update(title: params[:title], content: params[:content])
+end
+
+delete '/posts/:id' do
+  @post = Post.find_by(params[:id])
+  @post.destroy
+  redirect '/posts'
 end
 
 get '/comments' do
@@ -60,5 +139,14 @@ end
 get '/logout' do
   session[:user_id] = nil
 
-  redirect '/'
+  redirect '/dashboard'
 end
+
+def delete_user
+  # @current_user = User.find(session[:user_id])
+  # @current_user = destroy
+  # redirect '/users'
+end
+
+
+
--- models.rb
+++ models.rb
@@ -4,10 +4,18 @@ require 'pg'
 set :database, 'postgresql:blogs_data'
 
 class User < ActiveRecord::Base
+  has_many :posts, dependent: :destroy
+  has_many :comments
 end
 class Post < ActiveRecord::Base
+  belongs_to :user
+  has_many :comments
 end
 class Comment < ActiveRecord::Base
+  belongs_to :user
+  has_one :post
 end
 class Tag < ActiveRecord::Base
+  belongs_to :user
+  has_one :post
 end
